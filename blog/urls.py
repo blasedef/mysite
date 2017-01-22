cb909e75e98eeedb603225385d02338391401c94
cdw <blasedef@gmail.com>
2017-01-22 20:02:48 +0000

    chp1 complete

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
 	# post views
-	url(r'^$', views.post_list, name='post_list'),
+	# url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.PostListView.as_view(), name='post_list'),
 	url(r'^(?P<year>\d{4})/'\
 		r'(?P<month>\d{2})/'\
 		r'(?P<day>\d{2})/'\
