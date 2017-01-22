ccef2e6c00d265c9098b7f702841bc5c14aca16d
cdw <blasedef@gmail.com>
2017-01-22 19:20:28 +0000

    second chapter

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 # Create your models here.
+
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
 	STATUS_CHOICES = (
@@ -18,9 +23,19 @@ class Post(models.Model):
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+	objects = models.Manager()
+	published = PublishedManager()
 
 	class Meta:
 		ordering = ('-publish',)
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('blog:post_detail', args=[
+			self.publish.year,
+			self.publish.strftime('%m'),
+			self.publish.strftime('%d'),
+			self.slug
+			])	
